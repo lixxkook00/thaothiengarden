86d26c9fe9f5ca8a238333c31b5ab9394672924f
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-11-24 22:35:20 +0700

    first commit

---
+++ aboutUs/js/index.js
@@ -0,0 +1,45 @@
+/* 
+   INIT SCROLL EVENT
+*/
+//Header
+$(window).scroll(function () {
+   if ($(this).scrollTop() > 60) {
+      $("#header").addClass("scrolled");
+   } else {
+      $("#header").removeClass("scrolled");
+   }
+});
+
+/* 
+   TOGGLE LIGHT/DARK MODE
+*/
+$(function () {
+   $(".mode-dark").click(function () {
+      $("#data-theme").attr("data-theme", "dark");
+
+      $("#header").addClass("dark-theme");
+
+      $(".mode-dark").removeClass("active");
+      $(".mode-light").addClass("active");
+   });
+   $(".mode-light").click(function () {
+      $("#data-theme").removeAttr("data-theme");
+
+      $("#header").removeClass("dark-theme");
+
+      $(".mode-dark").addClass("active");
+      $(".mode-light").removeClass("active");
+   });
+
+   $(".icon-open").click(function () {
+      $("#header").addClass("show");
+      $(".icon-close").addClass("active");
+      $(".icon-open").removeClass("active");
+   });
+
+   $(".icon-close").click(function () {
+      $("#header").removeClass("show");
+      $(".icon-open").addClass("active");
+      $(".icon-close").removeClass("active");
+   });
+});
